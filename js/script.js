4f7d14a62158757b45ffbd529a6dcd667f4b8d54
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-04 16:53:50 +0100

    add 3 const

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/*
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
+//*Chiedi all’utente il suo nome,
+
+const username = prompt('Dammi il tuo nome');
+console.log(Text);
+
+//*Chiedi all’utente il suo cognome,
+
+const usersurname = prompt('Dammi il tuo cognome');
+console.log(Text);
+
+//*Chiedi all’utente il suo colore fav,
+
+const favcolor = prompt('Dammi il tuo colore favorito');
+
+le
