f1451daf3e325bcd8cc50fb5857f5163a1b4ff58
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-04 17:15:15 +0100

    add 23 number

--- js/script.js
+++ js/script.js
@@ -8,15 +8,27 @@ Infine scrivi sulla pagina nomecognomecolorepreferito23
 //*Chiedi all’utente il suo nome,
 
 const username = prompt('Dammi il tuo nome');
-console.log(Text);
+console.log(username);
 
 //*Chiedi all’utente il suo cognome,
 
 const usersurname = prompt('Dammi il tuo cognome');
-console.log(Text);
+console.log(usersurname);
 
 //*Chiedi all’utente il suo colore fav,
 
 const favcolor = prompt('Dammi il tuo colore favorito');
+console.log(favcolor);
 
-le
+//*add number 23,
+
+const number = ('23')
+console.log(number);
+
+//! Risultato
+
+const userMessage = username + usersurname + favcolor + number; 
+
+//TODO Infine scrivi sulla pagina nomecognomecolorepreferito23
+
+document.getElementById('your_info').innerHTML = userMessage;
